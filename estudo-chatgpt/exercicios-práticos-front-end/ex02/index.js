2862a8dd9080baedb1789d0a56fdfb0bb1b19cff
GabrielBerg4mini <gabrielbergaminioficial@gmail.com>
2024-01-14 15:52:36 -0300

    ajustando o readme

--- estudo-chatgpt/exercicios-práticos-front-end/ex02/index.js
+++ estudo-chatgpt/exercicios-práticos-front-end/ex02/index.js
@@ -5,89 +5,6 @@ const btnRegisterValid = document.querySelector(".button-register")
 
 const containerEmail = document.querySelector(".validEmail")
 
-btnRegisterValid.addEventListener("click", (event) => {
-  const inputs = document.querySelectorAll("input")
-  const nameUser = document.querySelector("#nome").value
-  const email = document.querySelector("#email2").value
-  const password2 = document.querySelector("#password2").value
-  const password3 = document.querySelector("#password3").value
-  const checkbox2 = document.querySelector("#register input[name='checkbox2']")
-  const containerNameRegister = document.querySelector(
-    ".containerValidName .validName"
-  )
-  const containerEmailRegister = document.querySelector(
-    ".containerValidEmail2 .validEmail2"
-  )
-  const containerPasswordRegister = document.querySelector(
-    ".containerValidPassword2 .validPassword2"
-  )
-  const containerCheckbox = document.querySelector(
-    ".containerValidCheckbox .validCheckbox"
-  )
-
-  if (nameUser.trim() === "") {
-    containerNameRegister.innerHTML = "Por favor, preencha o campo Nome."
-    inputs.forEach((input) => input.classList.add("teste"))
-    event.preventDefault()
-  }
-  if (!isValidEmail(email)) {
-    containerEmailRegister.style.color = "#ec2620"
-    containerEmailRegister.innerHTML = "Por favor, preencha o campo E-mail."
-    inputs.forEach((input) => input.classList.add("teste"))
-    event.preventDefault()
-  }
-
-  if (password2 === "" && password3 === "") {
-    containerPasswordRegister.style.color = "#ec2620"
-    containerPasswordRegister.innerHTML = "Por favor, preencha o campo Senha."
-    inputs.forEach((input) => input.classList.add("teste"))
-    event.preventDefault()
-    return
-  }
-
-  if (!checkbox2.checked) {
-    containerCheckbox.style.color = "#ec2620"
-    containerCheckbox.innerHTML =
-      "Por favor, concorde com os termos e condições."
-    event.preventDefault()
-    return
-  } else {
-    containerCheckbox.innerHTML = ""
-  }
-
-  const user = {
-    name: nameUser,
-    email: email,
-    password: password2,
-  }
-
-  users.push(user)
-
-  localStorage.setItem("users", JSON.stringify(users))
-
-  wrapper.classList.add("active")
-  checkPasswordMatch()
-})
-
-btnLoginValid.addEventListener("click", (event) => {
-  const loginEmail = document.getElementById("email").value
-  const loginPassword = document.getElementById("password").value
-
-  const user = users.find((user) => user.email === loginEmail)
-
-  if (user && user.password === loginPassword) {
-    containerEmail.innerHTML = "Login bem-sucedido."
-  } else {
-    containerEmail.innerHTML = "E-mail ou senha inválidos."
-
-    event.preventDefault()
-  }
-})
-function isValidEmail(email) {
-  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-  return emailRegex.test(email)
-}
-
 const passwordInput = document.querySelector("#password2")
 const passwordInput2 = document.querySelector("#password3")
 const passwordStrengthMessage = document.querySelector(
@@ -155,6 +72,123 @@ passwordInput.addEventListener("input", () => {
 
 passwordInput2.addEventListener("input", () => {
   checkPasswordMatch()
+})
+
+function isValidEmail(email) {
+  let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return emailRegex.test(email)
+}
+
+btnRegisterValid.addEventListener("click", (event) => {
+  const inputs = document.querySelectorAll("input")
+  const nameUser = document.querySelector("#nome").value
+  const email = document.querySelector("#email2").value
+  const password2 = document.querySelector("#password2").value
+  const password3 = document.querySelector("#password3").value
+  const checkbox2 = document.querySelector("#register input[name='checkbox2']")
+  const containerNameRegister = document.querySelector(
+    ".containerValidName .validName"
+  )
+  const containerEmailRegister = document.querySelector(
+    ".containerValidEmail2 .validEmail2"
+  )
+  const containerPasswordRegister = document.querySelector(
+    ".containerValidPassword2 .validPassword2"
+  )
+  const mensagemCheckbox = document.querySelector(".msgCheckbox")
+
+  if (nameUser.trim() === "") {
+    containerNameRegister.innerHTML = "Por favor, preencha o campo Nome."
+    inputs.forEach((input) => input.classList.add("teste"))
+    event.preventDefault()
+  }
+  if (!isValidEmail(email)) {
+    containerEmailRegister.style.color = "#ec2620"
+    containerEmailRegister.innerHTML = "Por favor, preencha o campo E-mail."
+    inputs.forEach((input) => input.classList.add("teste"))
+    event.preventDefault()
+  }
+
+  if (password2 === "" && password3 === "") {
+    containerPasswordRegister.style.color = "#ec2620"
+    containerPasswordRegister.innerHTML = "Por favor, preencha o campo Senha."
+    inputs.forEach((input) => input.classList.add("teste"))
+    event.preventDefault()
+    return
+  }
+
+  if (!checkbox2.checked) {
+    mensagemCheckbox.style.color = "#ec2620"
+    mensagemCheckbox.innerHTML =
+      "Por favor, concorde com os termos e condições."
+    event.preventDefault()
+    return
+  }
+
+  const user = {
+    name: nameUser,
+    email: email,
+    password: password2,
+  }
+
+  users.push(user)
+
+  localStorage.setItem("users", JSON.stringify(users))
+
+  wrapper.classList.add("active")
+  setTimeout(() => {
+    alert("Registrado com sucesso.")
+  }, 10)
+  checkPasswordMatch()
+})
+
+const checkboxRemember = document.getElementById("checkbox")
+
+window.addEventListener("load", () => {
+  const rememberedUser = JSON.parse(localStorage.getItem("rememberedUser"))
+
+  if (rememberedUser) {
+    document.getElementById("email").value = rememberedUser.email
+    document.getElementById("password").value = rememberedUser.password
+    checkboxRemember.checked = true
+  }
+})
+
+btnLoginValid.addEventListener("click", (event) => {
+  const loginEmail = document.getElementById("email").value
+  const loginPassword = document.getElementById("password").value
+
+  const user = users.find((user) => user.email === loginEmail)
+
+  if (user && user.password === loginPassword) {
+    containerEmail.innerHTML = "Login bem-sucedido."
+
+    if (checkboxRemember.checked) {
+      localStorage.setItem(
+        "rememberedUser",
+        JSON.stringify({ email: user.email, password: user.password })
+      )
+    }
+
+    event.preventDefault()
+
+    document.querySelector(".form-box.login").style.display = "none"
+
+    const successScreen = document.querySelector(".login-success")
+    successScreen.style.display = "grid"
+    successScreen.style.gap = "2rem"
+
+    const backToLoginButton = document.querySelector("#btnVoltarLogin")
+
+    backToLoginButton.addEventListener("click", () => {
+      successScreen.style.display = "none"
+      document.querySelector(".form-box.login").style.display = "block"
+    })
+    successScreen.appendChild(backToLoginButton)
+  } else {
+    containerEmail.innerHTML = "E-mail ou senha inválidos."
+    event.preventDefault()
+  }
 })
 
 const wrapper = document.querySelector(".wrapper")
